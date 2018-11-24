884c8239e2743ae02689e18121c162f3d27435b3
Daniel <szego.daniel@gmail.com>
2018-11-24 07:19:15 -0800

    naive order matching algorithm

--- contracts/OrderMatchingAbstract.sol
+++ contracts/OrderMatchingAbstract.sol
@@ -15,17 +15,33 @@ contract OrderMatchingAbstract {
     // order book    
     Order[] public orderBook;     
 
+    // EVENTS
+    event OrderAddedToBook(uint _amount,uint _price, bool _buyOrSell, string _asset);
+    event OrderDeletedFromBook(uint _amount,uint _price, bool _buyOrSell, string _asset);
+    event OrderMatch(uint _amountBuy,uint _priceBuy, string _assetBuy,
+                     uint _amountSell,uint _priceSell, string _assetSell);
+
+
     // ORDER BOOK CRUD FUNCTIONS
 
     // adding order to the order book
     function addOrder(uint _amount,uint _price, bool _buyOrSell, string _asset) public {
         Order memory newOrder = Order(_amount, _price, _buyOrSell, _asset);
         orderBook.push(newOrder);
+        emit OrderAddedToBook(_amount,_price,_buyOrSell,_asset);
+        // call mathing algorithm on top
+        matchOrder();
     }
 
     // deleting order from the order book - deleting can be called inly internally
     function deleteOrder(uint index) internal{
+        uint amount = orderBook[index].amount;
+        uint price = orderBook[index].price;
+        bool buyOrSell = orderBook[index].buyOrSell;
+        string asset = orderBook[index].asset;        
         delete orderBook[index];
+
+        emit OrderDeletedFromBook(amount,price,buyOrSell,asset);
     }
 
     // getting order details
@@ -42,7 +58,7 @@ contract OrderMatchingAbstract {
     } 
 
     // start order matching returning effected rows
-    function matchOrder() internal returns (uint);
+    function matchOrder() public returns (uint);
 
     
 
--- contracts/OrderMathcingSimple.sol
+++ contracts/OrderMathcingSimple.sol
@@ -11,8 +11,36 @@ contract OrderMatchingSimple is OrderMatchingAbstract {
    }
 
     // start order matching returning effected rows
-    function matchOrder() internal returns (uint) {
-        return 0;
-    }
+    function matchOrder() public returns (uint) {
 
+        for(uint actualBuyOrderIndex=0; actualBuyOrderIndex < orderBook.length; actualBuyOrderIndex++) {
+      	if (orderBook[actualBuyOrderIndex].buyOrSell == true) {
+		  	for(uint actualSellOrderIndex=0; actualSellOrderIndex < orderBook.length; actualSellOrderIndex++) {
+				if (orderBook[actualSellOrderIndex].buyOrSell == false) {
+                	// matching asset type 
+                  	if (sha256(orderBook[actualBuyOrderIndex].asset) == sha256(orderBook[actualSellOrderIndex].asset)) {       
+                        // matching amount
+	                  	if (orderBook[actualBuyOrderIndex].price >= orderBook[actualSellOrderIndex].price) {
+                            //MATCHING
+                            // Event
+                            emit OrderMatch(orderBook[actualBuyOrderIndex].amount,
+                             orderBook[actualBuyOrderIndex].price,
+                              orderBook[actualBuyOrderIndex].asset,
+                              orderBook[actualSellOrderIndex].amount,
+                              orderBook[actualSellOrderIndex].price,
+                              orderBook[actualSellOrderIndex].asset);
+
+                            // Applying to state
+
+                            // Deleting the orders
+                            deleteOrder(actualBuyOrderIndex);
+                            deleteOrder(actualSellOrderIndex);
+                        }                      	
+                    }                  	
+                }      		
+            }
+        }
+    }
 }
+}
+
